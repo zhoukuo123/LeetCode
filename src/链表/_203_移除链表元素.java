e720ef6e47fd743d58d41c97d6ee45531435c916
zhoukuo123 <1178053078@qq.com>
2020-03-31 10:58:52 +0800

    commmit

---
+++ src/链表/_203_移除链表元素.java
@@ -0,0 +1,10 @@
+package 链表;
+
+/**
+ * https://leetcode-cn.com/problems/remove-linked-list-elements/
+ */
+public class _203_移除链表元素 {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode
+    }
+}
