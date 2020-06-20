fa7a882fad3b09cee0e0b49235bb48453c5a57f8
zhoukuo123 <1178053078@qq.com>
2020-06-20 18:48:53 +0800

    update

--- src/链表/_876_链表的中间结点.java
+++ src/链表/_876_链表的中间结点.java
@@ -4,20 +4,39 @@ package 链表;
  * https://leetcode-cn.com/problems/middle-of-the-linked-list/
  */
 public class _876_链表的中间结点 {
-    public ListNode middleNode(ListNode head) {
+//    public ListNode middleNode(ListNode head) {
+//        ListNode slow = head;
+//        ListNode fast = head.next;
+//        int flag = 0;
+//        while (fast != null && fast.next != null && fast.next.next != null ) {
+//            flag = 1;
+//            fast = fast.next.next;
+//            slow = slow.next;
+//        }
+//        if (flag == 0 && fast == null) {
+//            head = slow;
+//        } else {
+//            head = slow.next;
+//        }
+//        return head;
+//    }
+
+    public ListNode middleNode2(ListNode head) {
+        if (head.next == null) return head;
+
         ListNode slow = head;
         ListNode fast = head.next;
-        int flag = 0;
-        while (fast != null && fast.next != null && fast.next.next != null ) {
-            flag = 1;
-            fast = fast.next.next;
+        while (fast.next != null) {
             slow = slow.next;
+            fast = fast.next.next;
+            if (fast == null) {
+                return slow;
+            }
+            if (fast.next == null) {
+                slow = slow.next;
+                return slow;
+            }
         }
-        if (flag == 0 && fast == null) {
-            head = slow;
-        } else {
-            head = slow.next;
-        }
-        return head;
+        return slow.next;
     }
 }
