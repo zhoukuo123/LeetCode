f19e2a026dfabb1f047996881b1193becb46746d
zhoukuo123 <1178053078@qq.com>
2020-10-21 22:16:53 +0800

    update: 2020/10/21

--- src/数组/_面试题_16_16_部分排序.java
+++ src/数组/_面试题_16_16_部分排序.java
@@ -21,6 +21,11 @@ public class _面试题_16_16_部分排序 {
             }
         }
 
+        // 提前结束
+        if (r == -1) {
+            return new int[]{-1, -1};
+        }
+
         // 从右扫描到左寻找逆序对(正序: 逐渐变小)
         int min = array[array.length - 1];
         int l = -1; // 用来记录最左的那个逆序对位置
