4bc75aeaa4cf563d4bc99f2f0e30c2d9d9bb8f01
zhoukuo123 <1178053078@qq.com>
2020-10-22 12:01:15 +0800

    update: 2020/10/22

--- src/链表/_203_移除链表元素.java
+++ src/链表/_203_移除链表元素.java
@@ -5,6 +5,54 @@ package 链表;
  */
 public class _203_移除链表元素 {
     public ListNode removeElements(ListNode head, int val) {
+        // O(n)
+        // 新链表的头结点
+        ListNode newHead = null;
+        // 新链表的尾结点
+        ListNode newTail = null;
+
+        while (head != null) {
+            if (head.val != val) {
+                if (newTail == null) {
+                    newHead = newTail = head;
+                } else {
+                    newTail.next = head;
+                    newTail = head;
+                }
+            }
+            head = head.next;
+        }
+        if (newTail == null) {
+            return null;
+        } else {
+            newTail.next = null;
+        }
+        return newHead;
+    }
+
+    public ListNode removeElements2(ListNode head, int val) {
+        // O(n)
+        // 添加虚拟头结点
+        if (head == null) {
+            return null;
+        }
+
+        ListNode newHead = new ListNode(0);
+        ListNode newTail = newHead;
+
+        while (head != null) {
+            if (head.val != val) {
+                newTail.next = head;
+                newTail = head;
+            }
+            head = head.next;
+        }
+        newTail.next = null;
+        return newHead.next;
+    }
+
+    public ListNode removeElements3(ListNode head, int val) {
+        // O(n)
         if (head == null) return null;
         while (head.val == val) {
             head = head.next;
