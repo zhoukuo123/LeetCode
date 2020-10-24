d7c1af97105b30a5f536ca99d2d058adc9bf12ec
zhoukuo123 <1178053078@qq.com>
2020-10-24 17:33:35 +0800

    update: 2020-10-24

--- src/链表/_234_回文链表.java
+++ src/链表/_234_回文链表.java
@@ -8,6 +8,8 @@ package 链表;
  */
 public class _234_回文链表 {
     public boolean isPalindrome(ListNode head) {
+        // 时间复杂度O(n), 空间复杂度O(1)
+        // 对称 -> 快慢指针找到中间节点
         if (head == null || head.next == null) {
             return true;
         }
@@ -15,6 +17,49 @@ public class _234_回文链表 {
             return head.val == head.next.val;
         }
 
+        // 找到中间节点
+        ListNode mid = middleNode(head);
+        // 翻转右半部分(中间节点的右边部分)
+        ListNode rHead = reverseList(mid.next);
+        ListNode lHead = head;
 
+        // 从lHead, rHead出发, 判断是否为回文链表
+        while (rHead != null) {
+            if (rHead.val != lHead.val) {
+                return false;
+            }
+            rHead = rHead.next;
+            lHead = lHead.next;
+        }
+        return true;
+    }
+
+    /**
+     * 找到中间节点(右半部分链表头节点的前一个节点)
+     * 比如 1>2>3>2>1中的3是中间节点
+     * 比如 1>2>2>1中左边第一个2是中间节点
+     */
+    private ListNode middleNode(ListNode head) {
+        ListNode fast = head.next;
+        ListNode slow = head;
+        while (fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == null) {
+                break;
+            }
+        }
+        return slow;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode tmp = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = tmp;
+        }
+        return newHead;
     }
 }
