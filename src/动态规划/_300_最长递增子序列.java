e386f92ba633ed7f9110a1257c01e53f0c15e670
zhoukuo123 <1178053078@qq.com>
2021-08-06 17:44:23 +0800

    update

---
+++ src/动态规划/_300_最长递增子序列.java
@@ -0,0 +1,30 @@
+package 动态规划;
+
+/**
+ * @author CoderZk
+ */
+public class _300_最长递增子序列 {
+    public static void main(String[] args) {
+
+    }
+
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int[] dp = new int[nums.length];
+        int max = dp[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] <= nums[j]) {
+                    continue;
+                }
+                dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+            max = Math.max(dp[i], max);
+        }
+        return max;
+    }
+}
