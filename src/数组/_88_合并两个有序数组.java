936728f383ae7d0abdce2171429b438461d0bffc
zhoukuo123 <1178053078@qq.com>
2021-08-17 17:45:42 +0800

    update

--- src/数组/_88_合并两个有序数组.java
+++ src/数组/_88_合并两个有序数组.java
@@ -10,7 +10,7 @@ public class _88_合并两个有序数组 {
         // 时间复杂度O(n)
         int i1 = m - 1;
         int i2 = n - 1;
-        int cur = m + n - 1;
+        int cur = nums1.length - 1;
 
         while (i2 >= 0) {
             if (i1 >= 0 && nums2[i2] < nums1[i1]) {
--- src/链表/_86_分隔链表.java
+++ src/链表/_86_分隔链表.java
@@ -7,6 +7,38 @@ package 链表;
  * @date 2020/10/22
  */
 public class _86_分隔链表 {
+    // 两个链表, 最终合并
+    public ListNode partition2(ListNode head, int x) {
+
+        if (head == null) {
+            return null;
+        }
+
+        ListNode lHead = new ListNode(0);
+        ListNode lTail = lHead;
+        ListNode rHead = new ListNode(0);
+        ListNode rTail = lHead;
+        ListNode cur = null;
+        int flag = 0;
+
+        while (head != null) {
+            if (head.val < x) {
+                lTail.next = head;
+                lTail = head;
+            } else {
+                rTail.next = head;
+                rTail = head;
+            }
+            head = head.next;
+        }
+
+        rTail.next = null;
+        lTail.next = rHead.next;
+
+        return lHead.next;
+    }
+
+
     public ListNode partition(ListNode head, int x) {
         // 时间复杂度O(n)
         // 空间复杂度O(1)
